470326e21bf8cf78d20d08c3442b641dac18f7e6
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-04-04 07:12:34 -0500

    example finished

--- Compare/file.js
+++ Compare/file.js
@@ -6,9 +6,34 @@ const app = new Vue({
         frutas:['Manzana', 'Pera', 'Platano'],
         //objeto
         perros:[
-            {nombre:'Mia', raza:'Labrador'},
-            {nombre:'Toby', raza:'none'},
-            {nombre:'Dante', raza:'Husky siberiano'}
-        ]
+            {nombre:'Mia', edad:1, raza:'Labrador'},
+            {nombre:'Toby', edad:2, raza:'none'},
+            {nombre:'Dante', edad:5, raza:'Husky siberiano'}
+        ],
+        nuevoPerro:'',
+        nuevaRaza:'',
+        edadPerro:'',
+        total:0
+    },
+    methods:{
+        agregarPerro() {
+            this.perros.push({
+                nombre:this.nuevoPerro,
+                edad:this.edadPerro,
+                raza:this.nuevaRaza
+            })
+            this.nuevoPerro = '';
+            this.edadPerro = '';
+            this.nuevaRaza = '';
+        }
+    },
+    computed:{
+        sumarEdades(){
+            this.total = 0;
+            for(perro of this.perros){
+                this.total = this.total + perro.edad;
+            }
+            return this.total;
+        }
     }
 }) 
