27746f24534c31a55b3869e7f5582723a138760f
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-04-27 11:26:36 -0500

    exercice vue added course of vue platzi

---
+++ exchange-coins/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <px-assets-table />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from '@/components/PxAssetsTable'
+import api from '@/api'
+
+export default {
+  name: 'Home',
+  components: { PxAssetsTable },
+  data() {
+    return {
+      assets: []
+    }
+  },
+  created() {
+    api.getAssets().then(assets => (this.assets = assets))
+  }
+}
+</script>
