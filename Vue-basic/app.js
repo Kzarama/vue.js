d02c9711c7bf4f37e1c1f5329792d142bc4c8844
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-07-20 22:45:54 -0500

    linter fixed

--- Vue-basic/app.js
+++ Vue-basic/app.js
@@ -1,53 +1,61 @@
-Vue.component('CoinDetail', {
-  props:['coin'],
-  
-  data(){
-    return{
-      showPrices:false,
-      value:0,
-    }
+Vue.component("CoinDetail", {
+  props: ["coin"],
+
+  data() {
+    return {
+      showPrices: false,
+      value: 0,
+    };
   },
-  
-  methods:{
-    toggleShowPrices(){
-      this.showPrices = !this.showPrices
-      this.$emit('change-color', 
-      this.showPrices ? 'FF96C8' : '3D3D3D')
-    }
-  },
-  
-  computed:{
-    title () {
-      return `${this.coin.name} - ${this.coin.symbol}`
+
+  methods: {
+    toggleShowPrices() {
+      this.showPrices = !this.showPrices;
+      this.$emit("change-color", this.showPrices ? "FF96C8" : "3D3D3D");
     },
-    convertedValue(){
-      if(!this.value){
-        return 0
+  },
+
+  computed: {
+    title() {
+      return `${this.coin.name} - ${this.coin.symbol}`;
+    },
+    convertedValue() {
+      if (!this.value) {
+        return 0;
       }
-      return this.value / this.coin.price
-    }
+      return this.value / this.coin.price;
+    },
   },
 
-  created(){
-    console.log('Created CoinDetail ...')
+  created() {
+    console.log("Created CoinDetail ...");
   },
 
-  mounted(){
-    console.log('Mounted CoinDetail ...')
+  mounted() {
+    console.log("Mounted CoinDetail ...");
   },
 
-  template:`
+  template: `
   <div>
-    <img v-on:mouseover="toggleShowPrices" v-on:mouseout="toggleShowPrices" v-bind:src="coin.img" v-bind:alt="coin.name">
+    <img 
+      v-on:mouseover="toggleShowPrices"
+      v-on:mouseout="toggleShowPrices"
+      v-bind:src="coin.img"
+      v-bind:alt="coin.name"
+    >
+    
     <h1 v-bind:class="coin.changePercent > 0 ? 'green' : 'red'">
       {{title}}
+
       <span v-if="coin.changePercent > 0">👍</span>
       <span v-else-if="coin.changePercent < 0">🤞</span>
       <span v-else>👎</span>
+
       <span v-on:click="toggleShowPrices">
         {{showPrices ? '🙈' : '🐵'}}
       </span>
     </h1>
+
     <input type="number" v-model="value">
     <span>{{convertedValue}}</span>
 
@@ -55,63 +63,63 @@ Vue.component('CoinDetail', {
     <slot name="link"></slot>
 
     <ul v-show=showPrices>
-      <li class="uppercase" v-bind:class="{orange:p.value === coin.price, red:p.value < coin.price, green:p.value > coin.price}" 
-      v-for="(p, i) in coin.pricesWithDays" 
-      v-bind:key="p.day">
-        {{i}} - {{p.day}} - {{p.value}}
+      <li
+        class="uppercase"
+        v-bind:class="{orange:p.value === coin.price, red:p.value < coin.price, green:p.value > coin.price}" 
+        v-for="(p, i) in coin.pricesWithDays" 
+        v-bind:key="p.day"
+      >
+          {{i}} - {{p.day}} - {{p.value}}
       </li>
     </ul>
   </div>
-`
-})
+`,
+});
 
 app = new Vue({
-  el:'#app',
-  data(){
+  el: "#app",
+  data() {
     return {
-      btc:{
-        name:'Bitcoin',
-        symbol:'BTC',
-        img:'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-        changePercent:10,
-        price:8400,
+      btc: {
+        name: "Bitcoin",
+        symbol: "BTC",
+        img: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+        changePercent: 10,
+        price: 8400,
         pricesWithDays: [
-          { day: 'Lunes', value: 8400 },
-          { day: 'Martes', value: 7900 },
-          { day: 'Miercoles', value: 8200 },
-          { day: 'Jueves', value: 9000 },
-          { day: 'Viernes', value: 9400 },
-          { day: 'Sabado', value: 10000 },
-          { day: 'Domingo', value: 10200 },
+          { day: "Lunes", value: 8400 },
+          { day: "Martes", value: 7900 },
+          { day: "Miercoles", value: 8200 },
+          { day: "Jueves", value: 9000 },
+          { day: "Viernes", value: 9400 },
+          { day: "Sabado", value: 10000 },
+          { day: "Domingo", value: 10200 },
         ],
       },
-      
-      color:'f4f4f4',
-    }
+
+      color: "f4f4f4",
+    };
   },
 
   // se ejecuta el crearse
-  created(){
-    console.log('Created ...')
+  created() {
+    console.log("Created ...");
   },
 
   // se ejecuta al montar
-  mounted(){
-    console.log('Mounted ...')
+  mounted() {
+    console.log("Mounted ...");
   },
 
-  methods:{
-    updateColor(color){
-      this.color = color || this.color.split('').reverse().join('')
-    }
+  methods: {
+    updateColor(color) {
+      this.color = color || this.color.split("").reverse().join("");
+    },
   },
-  
+
   // ejecuta el watch correspondiente cada vez q se cambia un atributo
   // cada metodo debe tener el nombre de un atributo
-  watch:{
-    showPrices(newVal, oldVal){
-
-    }
-  }
-
-})
+  watch: {
+    showPrices(newVal, oldVal) {},
+  },
+});
