7aa5c487440f45292d10da2d042f8f76c57908fa
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-07-24 21:39:39 -0500

    button to coin details

---
+++ criptocurrencies/src/components/PxButton.vue
@@ -0,0 +1,22 @@
+<template class="flex content-center">
+  <button
+    @click="buttonclick"
+    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold
+  hover:text-white py-2 px-2 border border-blue-500 hover:border-transparent
+  rounded"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PxButton',
+
+  methods: {
+    buttonclick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
