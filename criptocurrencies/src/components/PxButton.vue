9878591d54c59f0fc9a81fb750616a752ddd922d
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-07-25 07:10:43 -0500

    converter added

--- criptocurrencies/src/components/PxButton.vue
+++ criptocurrencies/src/components/PxButton.vue
@@ -1,12 +1,12 @@
 <template class="flex content-center">
   <button
     @click="buttonclick"
-    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold
-  hover:text-white py-2 px-2 border border-blue-500 hover:border-transparent
-  rounded"
+    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-2 border border-blue-500 hover:border-transparent rounded"
   >
     <beat-loader :loading="isLoading" :color="'#4DA8D7'" :size="8" />
-    <slot v-show="isLoading"></slot>
+    <p v-show="!isLoading">
+      <slot></slot>
+    </p>
   </button>
 </template>
 
