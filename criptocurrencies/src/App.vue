df227b0f0c8b249b1eed2094f9356ad179036c04
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-07-25 07:10:03 -0500

    links to coins added in the nav

--- criptocurrencies/src/App.vue
+++ criptocurrencies/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <PxHeader />
+    <PxHeader :links="links" />
     <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </main>
 </template>
@@ -10,7 +10,26 @@ import PxHeader from '@/components/PxHeader'
 
 export default {
   name: 'App',
-  components: { PxHeader }
+  components: { PxHeader },
+
+  data() {
+    return {
+      links: [
+        {
+          title: 'BTC',
+          to: { name: 'coin-detail', params: { id: 'bitcoin' } }
+        },
+        {
+          title: 'ETH',
+          to: { name: 'coin-detail', params: { id: 'ethereum' } }
+        },
+        {
+          title: 'USDT',
+          to: { name: 'coin-detail', params: { id: 'tether' } }
+        }
+      ]
+    }
+  }
 }
 </script>
 
